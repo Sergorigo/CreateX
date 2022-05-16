9f58218eed8ff16c260957ab30174632fda4841f
Sergorigo <98961442+Sergorigo@users.noreply.github.com>
2022-05-16 20:52:48 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,46 @@
+// создаю группу переменных
+// переменная элемента bullet
+let bullets = document.getElementsByClassName('bullet-navigation__item');
+// переменная карточки слайда
+let slides = document.getElementsByClassName('block-feedback__testimonal-container');
+// переменные для кнопок переключения слайдов
+let prevBtn = document.getElementById('button-left');
+let nextBtn = document.getElementById('button-right');
+let slideIndex = 1;
+// Теперь напишу функцию, которая будет показывать слайдер, в ней я создам несколько условных операторов.
+function showSlides(n) {
+   if (n < 1) {
+      slideIndex = slides.length;
+   } else if (n > slides.length) {
+      slideIndex = 1;
+   }
+   // Затем, прописываю циклы для скрытия неактивных слайдов и убрать класс active с буллетов
+   for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+   }
+   for (let i = 0; i < bullets.length; i++) {
+      bullets[i].classList.remove('active');
+   }
+   // Теперь нужно показать активный слайд, и привязать соответствующий класс к булету
+   slides[slideIndex - 1].style.display = 'block';
+   bullets[slideIndex - 1].classList.add('active');
+}
+
+// Теперь вызываю функцию и передаю ей параметр slideIndex
+showSlides(slideIndex);
+
+// Далее прописываю функцию, которая будет листать слайдер
+function plusSlides(n) {
+   // Вызываю функцию showSlides и добавляю к ней параметр n
+   showSlides(slideIndex += n);
+   setTimeout = 800;
+}
+
+// Осталось повесить на кнопки для перелистывания обработчики событий
+prevBtn.onclick = function () {
+   plusSlides(-1);
+}
+nextBtn.onclick = function () {
+   plusSlides(1);
+}
+
