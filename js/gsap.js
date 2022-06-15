3721ee7bfbfa73772426c7951e22862179286179
Sergorigo <98961442+Sergorigo@users.noreply.github.com>
2022-06-15 11:32:49 +0300

    new

---
+++ js/gsap.js
@@ -0,0 +1,6 @@
+gsap.from('.enjoy-studying__image', { duration: 1, x: '100%' })
+gsap.from('.block-text__title', { duration: 1, opacity: 0, delay: 0.5, ease: 'power1.in' })
+
+gsap.from('.about-us__image', { duration: 1, x: '100%' })
+gsap.from('.about-us__content-box', { duration: 1, opacity: 0, delay: 0.5, ease: 'power1.in' })
+
